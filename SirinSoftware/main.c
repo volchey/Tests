b873c6b9a2d3dd8aae910c79b0227fef78f58eda
Volodymyr <volchey07@gmail.com>
2018-09-30 16:08:49 +0300

    Sniff and put info to stdout

--- SirinSoftware/main.c
+++ SirinSoftware/main.c
@@ -13,20 +13,56 @@ int     count = 0;
 
 void    process_count(unsigned char *args, const struct pcap_pkthdr *header, const unsigned char *buffer)
 {
+struct sockaddr_in source;
     struct iphdr *iph = (struct iphdr*)(buffer + sizeof(struct ether_header));
-    printf("IP address: %s\n", inet_ntoa(iph->saddr) )
+source.sin_addr.s_addr = iph->saddr;
+    printf("IP address: %s\n", inet_ntoa(source.sin_addr) );
     count++;
 }
 
 int main() {
-    char    errbuf[100];
-    pcap_t  *handle;
-    handle = pcap_open_live("wlan1", 65536, 1, 0, errbuf);
-    if (handle == NULL)
+    pcap_if_t *alldevsp , *device;
+    pcap_t *handle; //Handle of the device that shall be sniffed
+ 
+    char errbuf[100] , *devname , devs[100][100];
+    int count = 1 , n;
+     
+    //First get the list of available devices
+    printf("Finding available devices ... ");
+    if( pcap_findalldevs( &alldevsp , errbuf) )
     {
-        printf("%s\n", errbuf);
+        printf("Error finding devices : %s" , errbuf);
         exit(1);
     }
+    printf("Done");
+     
+    //Print the available devices
+    printf("\nAvailable Devices are :\n");
+    for(device = alldevsp ; device != NULL ; device = device->next)
+    {
+        printf("%d. %s - %s\n" , count , device->name , device->description);
+        if(device->name != NULL)
+        {
+            strcpy(devs[count] , device->name);
+        }
+        count++;
+    }
+     
+    //Ask user which device to sniff
+    printf("Enter the number of the device you want to sniff : ");
+    scanf("%d" , &n);
+    devname = devs[n];
+     
+    //Open the device for sniffing
+    printf("Opening device %s for sniffing ... " , devname);
+    handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
+     
+    if (handle == NULL) 
+    {
+        fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+        exit(1);
+    }
+    printf("Done\n");
     pcap_loop(handle, 5, process_count, NULL);
     printf("packets count = %d\n", count);
     return 0;
