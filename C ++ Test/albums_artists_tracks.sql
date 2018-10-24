52a1a0bf84596351bc7043224a45c8577dcb9988
Volodymyr CHECHAILUIK <vchechai@e1r6p9.unit.ua>
2018-10-24 13:20:06 +0300

    sql test done

--- C ++ Test/albums_artists_tracks.sql
+++ C ++ Test/albums_artists_tracks.sql
@@ -1,22 +1,25 @@
-
-
 # Нужно создать структуру таблиц: Albums, Artists, Tracks.
 # В альбоме может быть много треков. У каждого трека может быть один или несколько авторов.
 # Нужно написать запрос для выбора альбомов в записи которых учатвовало больше одного исполнителя.
 
 CREATE TABLE Albums (
-	AlbumID		int,
-	AlbumName	varchar(255)
+	AlbumID		int NOT NULL AUTO_INCREMENT,
+	AlbumName	varchar(255),
+	PRIMARY KEY  (AlbumID)
 );
 
 CREATE TABLE Artists (
-
+	ArtistID	int NOT NULL AUTO_INCREMENT,
+	ArtistName	varchar(255),
+	PRIMARY KEY  (ArtistID)
 );
 
 CREATE TABLE Tracks (
-	TrackID		int,
-	TrackName	varchar(255),
-	TrackArtist	varchar(255)
+	TrackID				int NOT NULL AUTO_INCREMENT,
+	TrackName			varchar(255),
+	TrackArtistCount	int NOT NULL,
+	TrackAlbum			varchar(255),
+	PRIMARY KEY         (TrackID)
 );
 
-SELECT * FROM Albums WHERE ;
+SELECT * FROM Albums WHERE (AlbumName=(SELECT TrackAlbum FROM Tracks)) AND ((SELECT TrackArtistCount FROM Tracks) > 1);
